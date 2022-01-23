ae3d7b8e5143d90c328adc15583d376c83ad00f1
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-01-22 21:38:43 -0800

    Add initial files and structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/contact/Contact';
+import Home from './components/home/Home';
+import Projects from './components/projects/Projects';
+import Skills from './components/skills/Skills';
+import Navbar from './navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }

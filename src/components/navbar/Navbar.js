ac0de30f8afb20451467e01107f8a4742c334110
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-01-23 13:34:59 -0800

    Style navbar and create nav links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,18 @@
+import { Link, animateScroll as scroll } from 'react-scroll';
+
 import './Navbar.css';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="nav-content">
+        <Link className="brand">JYL</Link>
+        <ul className="nav-items">
+          <Link className="nav-item">Skills</Link>
+          <Link className="nav-item">Projects</Link>
+          <Link className="nav-item">Contact</Link>
+        </ul>
+      </div>
+    </nav>
+  );
 }

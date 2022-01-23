4fa71c213db0cdbc3ebb82394e4f040ceb271397
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-01-22 21:42:41 -0800

    Move navbar folder to correct location

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from './components/contact/Contact';
 import Home from './components/home/Home';
 import Projects from './components/projects/Projects';
 import Skills from './components/skills/Skills';
-import Navbar from './navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import './Navbar.css';
 
 export default function Navbar() {
-  return <div>Nav</div>;
+  return <div>Navbar</div>;
 }

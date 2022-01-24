4af4a6b895618ff1b6d77d1895fc6447b61d6272
J. Yasmeen Lisondra <jylisondra@gmail.com>
2022-01-23 18:40:43 -0800

    Center content with flexbox

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,11 +2,13 @@ import './Home.css';
 
 export default function Home() {
   return (
-    <div className="home">
+    <div className="home" id="home-section">
       <h1>Hi, I'm Joelle Lisondra</h1>
       <p>Full Stack Developer</p>
-      <button className="btn-contact">Contact Me</button>
-      <button className="btn-resume">View Resume</button>
+      <div className="btn-list">
+        <button className="btn-contact">Contact Me</button>
+        <button className="btn-resume">View Resume</button>
+      </div>
     </div>
   );
 }
